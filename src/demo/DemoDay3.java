ddf0535d1effc478a446d8dcc77880560b0229aa
ajmal <ajmal@localhost>
2023-07-30 12:29:40 -0400

    ajmal

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -3,4 +3,25 @@ package demo;
 public class DemoDay3 {
 
 //importance of following stash process && communicating... :)
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	
+	}
+	
+	
+	public void add () {
+		
+		int a = 50;
+		int b = 50;
+		int c = 50;
+		int d = 50;
+		
+		int result = a+b+c+d;
+		System.out.println ("Iphone: " + result);
+		
+	}
 }
